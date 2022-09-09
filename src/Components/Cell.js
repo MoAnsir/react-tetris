d0ab001eb79b608e6affdeda3a362ced1ff27dad
Mohammed Ansr <mohammed.ansir@gmail.com>
2022-09-09 19:33:41 +0100

    styling changes

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -4,7 +4,7 @@ import { TETRISBLOCKS } from "../JS/tetrisBlocks";
 import "../CSS/Cell.scss";
 
 const Cell = ({ type }) => {
-  return <div className={`cell_${type}`}></div>;
+  return <div className={`cell cell_${type}`}></div>;
 };
 
 export default Cell;
